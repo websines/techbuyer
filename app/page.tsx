4a9a9c22eb0d399c262f52fbfa91caf1c599ccb6
websines <to.subh03@gmail.com>
2024-10-01 03:18:21 +0530

    update-3

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import Services from "@/components/page-components/Services";
 
 export default function Home() {
   return (
-    <div className="overflow-clip top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <div className="min-h-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
       <Navbar />
-      <Hero />
-      <Services />
+      <main className="container mx-auto">
+        <Hero />
+        <Services />
+      </main>
     </div>
   );
 }
--- components/page-components/Services.tsx
+++ components/page-components/Services.tsx
@@ -15,7 +15,7 @@ const Services = () => {
           Services
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[450px] gap-4">
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
             <Image
               src={"/images/image-1.jpg"}
               width={1000}
@@ -35,7 +35,7 @@ const Services = () => {
               Get a quote
             </Link>
           </div>
-          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[450px] gap-4">
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
             <Image
               src={"/images/image-1.jpg"}
               width={1000}
@@ -55,7 +55,7 @@ const Services = () => {
               Get a quote
             </Link>
           </div>
-          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[450px] gap-4">
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
             <Image
               src={"/images/image-1.jpg"}
               width={1000}
@@ -75,7 +75,7 @@ const Services = () => {
               Get a quote
             </Link>
           </div>
-          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[450px] gap-4">
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
             <Image
               src={"/images/image-1.jpg"}
               width={1000}
@@ -95,7 +95,7 @@ const Services = () => {
               Get a quote
             </Link>
           </div>
-          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[450px] gap-4">
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
             <Image
               src={"/images/image-1.jpg"}
               width={1000}
@@ -115,7 +115,67 @@ const Services = () => {
               Get a quote
             </Link>
           </div>
-          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[450px] gap-4">
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
+            <Image
+              src={"/images/image-1.jpg"}
+              width={1000}
+              height={1000}
+              alt="service-1"
+              className="w-[450px] max-h-[250px]"
+            />
+            <span className="text-3xl font-medium">Servers</span>
+            <p className="text-sm font-medium leading-6 p-2">
+              Including rack, blade and tower server models. We accept a variety
+              of brands including HPE, Dell and IBM.
+            </p>
+            <Link
+              href={"/contact"}
+              className="my-2 px-8 py-2 w-full border-2 border-black dark:border-white uppercase bg-white hover:bg-blue-500 text-black transition duration-200  hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] text-lg text-center hover:text-white font-semibold"
+            >
+              Get a quote
+            </Link>
+          </div>
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
+            <Image
+              src={"/images/image-1.jpg"}
+              width={1000}
+              height={1000}
+              alt="service-1"
+              className="w-[450px] max-h-[250px]"
+            />
+            <span className="text-3xl font-medium">Servers</span>
+            <p className="text-sm font-medium leading-6 p-2">
+              Including rack, blade and tower server models. We accept a variety
+              of brands including HPE, Dell and IBM.
+            </p>
+            <Link
+              href={"/contact"}
+              className="my-2 px-8 py-2 w-full border-2 border-black dark:border-white uppercase bg-white hover:bg-blue-500 text-black transition duration-200  hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] text-lg text-center hover:text-white font-semibold"
+            >
+              Get a quote
+            </Link>
+          </div>
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
+            <Image
+              src={"/images/image-1.jpg"}
+              width={1000}
+              height={1000}
+              alt="service-1"
+              className="w-[450px] max-h-[250px]"
+            />
+            <span className="text-3xl font-medium">Servers</span>
+            <p className="text-sm font-medium leading-6 p-2">
+              Including rack, blade and tower server models. We accept a variety
+              of brands including HPE, Dell and IBM.
+            </p>
+            <Link
+              href={"/contact"}
+              className="my-2 px-8 py-2 w-full border-2 border-black dark:border-white uppercase bg-white hover:bg-blue-500 text-black transition duration-200  hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] text-lg text-center hover:text-white font-semibold"
+            >
+              Get a quote
+            </Link>
+          </div>
+          <div className="rounded-[6px] bg-[#fafafa] flex flex-col items-center justify-center p-4 w-[350px] gap-4">
             <Image
               src={"/images/image-1.jpg"}
               width={1000}
