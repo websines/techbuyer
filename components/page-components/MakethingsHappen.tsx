1c23c99e39213c1a00d0929b52bddd1609a5992c
websines <to.subh03@gmail.com>
2024-10-03 00:28:48 +0530

    link-fix

--- components/page-components/MakethingsHappen.tsx
+++ components/page-components/MakethingsHappen.tsx
@@ -17,7 +17,7 @@ function LetsMakeThingsHappenSection() {
         </p>
 
         <Link
-          href="/meeting"
+          href="/contact"
           className="py-3 
             px-10
         
@@ -34,7 +34,7 @@ function LetsMakeThingsHappenSection() {
        duration-200 
        hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
         >
-          Book a Call
+          Get a quote
         </Link>
       </div>
       <div className="absolute -top-8 right-8 hidden md:block">
